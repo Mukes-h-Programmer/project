5af11290ed3fa6ed93d31dc7cc862643f2854702
Mukes-h-Programmer <midasmukesh6@gmail.com>
2022-03-10 00:52:54 +0530

    complete login page design

---
+++ 7th day of 9th March/c.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+int x = 01234;
+printf(“%d”, x);
+return 0;
+}
---
+++ 7th day of 9th March/login.js
@@ -0,0 +1,31 @@
+function validateform() {
+    var name = document.myform.name.value;
+    var password = document.myform.password.value;
+
+    if (name == null || name == "") {
+        alert("Name can't be blank");
+        return false;
+    }
+    else if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        return false;
+    }
+}
+
+function profile() {
+    document.getElementById("myDropdown").classList.toggle("shoow");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(evetn) {
+    if (!evetn.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName;("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shoow')) {
+          openDropdown.classList.remove('shoow');
+        }
+      }
+    }
+  }
